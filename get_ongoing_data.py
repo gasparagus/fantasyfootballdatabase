650c089a689616074f05090129c7a10650c718b9
gasparagus <gary2g99@gmail.com>
2017-09-13 19:08:32 -0400

    Add files via upload

--- get_ongoing_data.py
+++ get_ongoing_data.py
@@ -3,6 +3,7 @@ import csv
 import os
 import remove_columns as rc
 import datetime as dt
+import postgres_ongoing_additions as pg
 
 # Try this once the current season starts. The 2017 season starts on 9/7/2017 which is week 36
 # but the final game of the week (Monday) is on week 37. You should probably run the script on
@@ -18,7 +19,9 @@ week_2017 = [x for x in range(37,53)]
 week_2017.append(1)
 week_mappings = zip(week_2017,nfl_season_weeks)
 
-current_nfl_week = [week[1] for week in week_mappings if week[0] == current_week_number]
+for week in week_mappings:
+	if week[0] == current_week_number:
+		current_nfl_week= week[1]
 
 # Delete next line if script works in the beginning of season.
 #nflgame.combine_max_stats(nflgame.games(current_year, week=week_number)).csv(str(current_year)+'_'+str(week_number)+'.csv',allfields=True)
@@ -27,3 +30,4 @@ nflgame.combine_max_stats(nflgame.games(current_year, week=current_nfl_week)).cs
 
 # Run cleanup of columns.
 rc.clean_columns()
+pg.load_table()
--- postgres_ongoing_additions.py
+++ postgres_ongoing_additions.py
@@ -3,23 +3,25 @@ import psycopg2.extras
 import os
 import shutil
 
-try:
-	conn = psycopg2.connect(dbname='fantasy',host='localhost',user='postgres')
-	print 'Connected'
-except:
-	print 'Cant connect'
+def load_table():
+	try:
+		conn = psycopg2.connect(dbname='fantasy',host='localhost',user='postgres')
+		print 'Connected'
+	except:
+		print 'Cant connect'
 
-cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+	cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-path = 'C:\Users\Gary\Documents\\nfl\\all_data_max'
+	path = 'C:\Users\Gary\Documents\\nfl\\all_data_max'
 
-for file in os.listdir(path):
-	if file.startswith('20'):
-		data_import_sql = 'copy ' + 'fantasy.stats FROM \'C:\Users\Gary\Documents\\nfl\\all_data_max\\' + str(file) + '\' DELIMITER \',\' CSV HEADER;'
-		cur.execute(data_import_sql)
-		conn.commit()
-	conn.close()
-	shutil.move('C:\Users\Gary\Documents\\nfl\\all_data_max\\'+file,
-	'C:\Users\Gary\Documents\\nfl\\all_data_max\\clean_files\\'+file)
+	# Currently stats test - change to stats after
+	for file in os.listdir(path):
+		if file.startswith('20'):
+			data_import_sql = 'copy ' + 'fantasy.stats FROM \'C:\Users\Gary\Documents\\nfl\\all_data_max\\' + str(file) + '\' DELIMITER \',\' CSV HEADER;'
+			cur.execute(data_import_sql)
+			conn.commit()
+		conn.close()
+		shutil.move('C:\Users\Gary\Documents\\nfl\\all_data_max\\'+file,
+		'C:\Users\Gary\Documents\\nfl\\all_data_max\\clean_files\\'+file)
 
 
