82a149e8273c64a70f85fc15b5c0424c7fa4486b
gasparagus <gary2g99@gmail.com>
2018-10-16 19:06:47 -0400

    Add files via upload

--- get_all_player_data_full_refresh.py
+++ get_all_player_data_full_refresh.py
@@ -4,16 +4,24 @@ import os
 import remove_columns as rc
 import datetime as dt
 import postgres_ongoing_additions as pg
+import pandas as pd
 
 current_year = dt.datetime.today().year
 # Year set to current so so that truncate and reload only happens for current year
-years = [x for x in range(2018, current_year)]
+years = [x for x in range(2018, current_year+1)]
 weeks = [x for x in range(1,18)]
 
 for year in years:
 	for week in weeks:
-		nflgame.combine_max_stats(nflgame.games(year, week=week)).csv(str(year)+'_'+str(week)+'.csv',allfields=True)
+		try:
+			nflgame.combine_max_stats(nflgame.games(year, week=week)).csv(str(year)+'_'+str(week)+'.csv',allfields=True)
+		except:
+			pass
 
 rc.clean_columns()
 pg.load_table()
 pg.update_points()
+
+# Dump data to desktop
+data = pd.read_sql('select * from fantasy.stats where year = 2018 and pos in (\'RB\', \'QB\', \'WR\', \'TE\')', con='postgresql://postgres:labella@localhost:5432/fantasy')
+data.to_csv('C:\Users\Gary\Desktop\\2018_nfl_data.csv', index=False)
